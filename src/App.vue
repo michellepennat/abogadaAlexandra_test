609cfb21e8c179c4444a293ed902684ff517da66
michellepennat <mp.design.apps@gmail.com>
2023-02-05 18:48:56 -0500

    feat(): add login

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Login />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Login from "./pages/Login/Login.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
